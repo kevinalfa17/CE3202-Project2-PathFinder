aef77477133a3761ce9ac56070780163127ac234
Daedgomez <daedgomez@gmail.com>
2017-10-16 18:50:42 -0600

    [Fix] Conflicts

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,10 @@
 
 #include "config.h"
 #include "Matrix/Matrix.hpp"
-<<<<<<< HEAD:src/CE3202-Project2-PathFinder.cpp
 #include "Matrix/MatrixDescomposition.h"
-#include "NodePair.h"
-#include "IndexMap.h"
-=======
 #include "PathFinder/NodePair.h"
 #include "PathFinder/IndexMap.h"
 #include "PathFinder/PathFinder.h"
->>>>>>> a86450cd7fdef945c58bd22fa5b225c7f326c895:src/main.cpp
 #include <iostream>
 #include <boost/bimap.hpp>
 #include <boost/bimap/unordered_set_of.hpp>
@@ -50,7 +45,7 @@ void printMatrix(anpi::Matrix<T> &m){
 	}
 }
 
-<<<<<<< HEAD:src/CE3202-Project2-PathFinder.cpp
+
 template <typename T>
 void printVector(vector<T> &v){
 	cout << "[\t";
@@ -58,22 +53,9 @@ void printVector(vector<T> &v){
 		cout << v.at(i) << "\t";
 	}
 	cout << "]" << endl;
-=======
-void fillHoles(Mat &mask)
-{
-    /*
-     This hole filling algorithm is decribed in this post
-     https://www.learnopencv.com/filling-holes-in-an-image-using-opencv-python-c/
-     */
 
-    Mat maskFloodfill = mask.clone();
-    floodFill(maskFloodfill, cv::Point(0,0), Scalar(255));
-    Mat mask2;
-    bitwise_not(maskFloodfill, mask2);
-    mask = (mask2 | mask);
-
->>>>>>> a86450cd7fdef945c58bd22fa5b225c7f326c895:src/main.cpp
 }
+
 
 /*template< class MapType >
 void print_map(const MapType & map)
@@ -93,10 +75,10 @@ void print_map(const MapType & map)
 int main() {
 	cout << "!!!Hello World SIMD is active and available!!!!" << endl; // prints !!!Hello World!!!
 	UploadImage * uploadImage = new UploadImage();
-	
+	/**
 
 
-	/*//SSE Example: calculate sqrt(x)/x for values from 1 to 400
+	//SSE Example: calculate sqrt(x)/x for values from 1 to 400
 	int length = 400;
 	float* pResult = (float*) _mm_malloc (length * sizeof(float), 16);//Align 400 float spaces to 16-byte for SSE
 
@@ -140,8 +122,8 @@ int main() {
 
 	NodePair pair = indexMap->getNodesFromX(22); //Get nodes terminals of x
 	cout << "pair" << endl;
-	pair.printPair();*/
-
+	pair.printPair();
+*/
 
 
 	Matrix<double> M_1 = Matrix<double>(4, 4, Matrix<double>::Padded);
@@ -172,13 +154,12 @@ int main() {
 	vector<double> x;
 
 
-<<<<<<< HEAD:src/CE3202-Project2-PathFinder.cpp
+
 	Matrix<double> M_2 = Matrix<double>(3, 3, Matrix<double>::Padded);
 	MatrixDescomposition<double> *ss = new MatrixDescomposition<double>();
 	//ss->lu(M_1, M_2);
 	ss->solveLU(M_1, x, b);
 	printVector(x);
-=======
 	//Initializer list test
 	//Matrix<int> B = {{1,2,3,4,5,6,7,8,9,10,11},{1,2,3,4,5,6,7,8,9,10,11},{1,2,3,4,5,6,7,8,9,10,11},{1,2,3,4,5,6,7,8,9,10,11}};
 	//printMatrix(B);
@@ -188,10 +169,6 @@ int main() {
 	PathFinder<float>  * pathFinder = new PathFinder<float>(0,0,2,2,uploadImage->getImage());
 	Matrix<float> A = pathFinder->getA();
 	printMatrix(A);
->>>>>>> a86450cd7fdef945c58bd22fa5b225c7f326c895:src/main.cpp
-
-	
-	
 	return 0;
 }
 #else
